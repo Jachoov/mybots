5a51bc7e72696093b29fd8d8c4f1218499ead192
tjoe <tjoe@uvm.edu>
2024-01-29 19:58:42 -0500

    Starting work on simulation p1.

--- OneDrive/Desktop/CS3060/simulate.py
+++ OneDrive/Desktop/CS3060/simulate.py
@@ -1,1 +1,9 @@
-pass
+import pybullet as p
+import time
+physicsClient = p.connect(p.GUI)
+# p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+for i in range(1000):
+    time.sleep(1/60)
+    p.stepSimulation()
+    print(i)
+p.disconnect()
