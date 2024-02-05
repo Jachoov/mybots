63617bc181514cdc524b279ee537130ba8cd9525
tjoe <tjoe@uvm.edu>
2024-02-05 18:50:11 -0500

    minimal

--- OneDrive/Desktop/CS3060/generate.py
+++ OneDrive/Desktop/CS3060/generate.py
@@ -1,14 +1,11 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("boxes.sdf")
+pyrosim.Start_SDF("world.sdf")
 length = 1
 width = 1
 height = 1
 x = 0
 y = 0
 z = 0.5
-x1 = 0
-y1 = 1
-z1 = 1.5
 pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-pyrosim.Send_Cube(name="Box2", pos=[x1,y1,z1] , size=[length,width,height])
+
 pyrosim.End()
