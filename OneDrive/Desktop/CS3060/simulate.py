982088cc6d8c62ab597c32d0c2a0b3edfb486ac1
tjoe <tjoe@uvm.edu>
2024-02-12 20:13:57 -0500

    Saving sensor values

--- OneDrive/Desktop/CS3060/simulate.py
+++ OneDrive/Desktop/CS3060/simulate.py
@@ -1,6 +1,7 @@
 import pybullet_data
 import pybullet as p
 import pyrosim.pyrosim as pyrosim
+import numpy
 import time
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -10,9 +11,10 @@ planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
-for i in range(1000):
+backLegSensorValues = numpy.zeros(10)
+for i in range(10):
     time.sleep(1/60)
     p.stepSimulation()
-    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("Backleg")
-    print(i)
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("Backleg")
 p.disconnect()
+numpy.save('data/Backleg_touch.npy',backLegSensorValues)
