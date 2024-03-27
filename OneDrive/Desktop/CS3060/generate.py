4a2c4a3b43c7edc56658c024f19f36358f42fc0c
tjoe <tjoe@uvm.edu>
2024-03-27 15:21:06 -0400

    big late save

--- OneDrive/Desktop/CS3060/generate.py
+++ OneDrive/Desktop/CS3060/generate.py
@@ -1,4 +1,5 @@
 import pyrosim.pyrosim as pyrosim
+import random
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
     length = 1
@@ -9,14 +10,45 @@ def Create_World():
     z = 0.5
     pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
     pyrosim.End()
-def Create_Robot():
+def Generate_Body():
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name="Torso", pos=[1.5,0,1.5] , size=[1,1,1])
     pyrosim.Send_Joint( name = "Torso_Backleg" , parent= "Torso" , child = "Backleg" , type = "revolute", position = [1,0,1])
     pyrosim.Send_Cube(name="Backleg", pos=[-0.5,0,-0.5] , size=[1,1,1])
     pyrosim.Send_Joint( name = "Torso_Frontleg" , parent= "Torso" , child = "Frontleg" , type = "revolute", position = [2,0,1])
     pyrosim.Send_Cube(name="Frontleg", pos=[.5,0,-0.5] , size=[1,1,1])
-    pyrosim.End()    
+    pyrosim.End()
+def Generate_Brain():
+    pyrosim.Start_NeuralNetwork("brain.nndf")
+    pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
+    pyrosim.Send_Sensor_Neuron(name = 1 , linkName = "Backleg")
+    pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "Frontleg")
+    pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_Backleg")
+    pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_Frontleg")
+
+    
+    # pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 3 , weight = 1.0 )
+    # pyrosim.Send_Synapse( sourceNeuronName = 2 , targetNeuronName = 3 , weight = 2.0 )
+    # pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 3 , weight = 1.0 )
+    # pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 4 , weight = -3.0 )
+    
+    # pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 3 , weight = 1.0 )
+    # pyrosim.Send_Synapse( sourceNeuronName = 2 , targetNeuronName = 4 , weight = -2.0 )
+    # pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 3 , weight = 1.0 )
+
+    
+    # pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 3 , weight = 1.0 )
+    # pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 3 , weight = 2.0 )
+    # pyrosim.Send_Synapse( sourceNeuronName = 2 , targetNeuronName = 4 , weight = 2.0 )
+    sensorNeurons = [0,1,2]
+    motorNeurons = [3,4]
+    for i in sensorNeurons:
+        for j in motorNeurons:
+            pyrosim.Send_Synapse( sourceNeuronName = i , targetNeuronName = j , weight = random.randrange(-1,1) )
+        
+    pyrosim.End()
 
 Create_World()
-Create_Robot()
+# Create_Robot()
+Generate_Body()
+Generate_Brain()
--- OneDrive/Desktop/CS3060/simulate.py
+++ OneDrive/Desktop/CS3060/simulate.py
@@ -1,23 +1,11 @@
-import pybullet_data
-import pybullet as p
-import pyrosim.pyrosim as pyrosim
-import numpy
-import time
-physicsClient = p.connect(p.GUI)
-p.setAdditionalSearchPath(pybullet_data.getDataPath())
-# p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
-p.setGravity(0,0,-9.8)
-planeId = p.loadURDF("plane.urdf")
-robotId = p.loadURDF("body.urdf")
-p.loadSDF("world.sdf")
-pyrosim.Prepare_To_Simulate(robotId)
-backLegSensorValues = numpy.zeros(100)
-frontLegSensorValues = numpy.zeros(100)
-for i in range(100):
-    time.sleep(1/60)
-    p.stepSimulation()
-    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("Backleg")
-    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("Frontleg")
-p.disconnect()
-numpy.save('data/backLegSensorValues.npy',backLegSensorValues)
-numpy.save('data/frontLegSensorValues.npy',frontLegSensorValues)
+from simulation import SIMULATION
+import sys
+
+# python simulate.py DIRECT
+# python simulate.py GUI
+directOrGUI = sys.argv[1]
+solutionID = sys.argv[2]
+
+simulation = SIMULATION(directOrGUI,solutionID)
+simulation.Run()
+simulation.Get_Fitness()
